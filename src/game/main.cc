8eff370b0119c5f1dabeae58df7847d4971eeabb
Sean <pixelpicosean@gmail.com>
2014-10-19 14:11:54 +0800

    Make code cleaner by move init and render out of main

--- src/game/main.cc
+++ src/game/main.cc
@@ -1,29 +1,50 @@
 #include <SDL2/SDL.h>
 
-SDL_Window *g_pWindow = 0;
-SDL_Renderer *g_pRenderer = 0;
+SDL_Window *g_pWindow = nullptr;
+SDL_Renderer *g_pRenderer = nullptr;
+
+bool g_bRunning = false;
+
+bool init(const char *title, int width, int height)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
+        g_pWindow = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN);
+
+        if (g_pWindow != nullptr) {
+            g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+        }
+    }
+    else {
+        return false;
+    }
+
+    return true;
+}
+
+void render()
+{
+    // Set to black
+    SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+
+    // Clear window
+    SDL_RenderClear(g_pRenderer);
+
+    // Render to window
+    SDL_RenderPresent(g_pRenderer);
+}
 
 int main(int argc, char *argv[])
 {
-    // init SDL
-    if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
-        g_pWindow = SDL_CreateWindow("Hello", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 960, 640, SDL_WINDOW_SHOWN);
-
-        if (g_pWindow != 0) {
-            g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-        }
+    if (init("Awesome Game", 640, 480)) {
+        g_bRunning = true;
     }
     else {
         return -1;
     }
 
-    SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-
-    SDL_RenderClear(g_pRenderer);
-
-    SDL_RenderPresent(g_pRenderer);
-
-    SDL_Delay(5000);
+    while (g_bRunning) {
+        render();
+    }
 
     SDL_Quit();
 
